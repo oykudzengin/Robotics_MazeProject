76974f66f6bd89cd69f89a8dbb012265a186925f
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-04-23 17:40:23 +0200

    test

--- src/silver_fundamentals/src/laser_server.cpp
+++ src/silver_fundamentals/src/laser_server.cpp
@@ -4,8 +4,16 @@
 #include <cstdlib>
 
 #include <cstring>
+#include <bits/streambuf_iterator.h>
 
-static float scan_data[726];
+static struct laser_data
+{
+    float ranges[726];
+    float min_angle;
+    float max_angle;
+    float angle_increment;
+} laser_data;
+
 static int count = 0;
 
 bool complete_laser_data(silver_fundamentals::Laser::Request  &req,
@@ -15,11 +23,14 @@ bool complete_laser_data(silver_fundamentals::Laser::Request  &req,
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-    std::copy(msg->ranges.begin(), msg->ranges.end(), scan_data);
+    std::copy(msg->ranges.begin(), msg->ranges.end(), laser_data.ranges);
+    laser_data.min_angle = msg->min_angle;
+    laser_data.max_angle = msg->max_angle;
+    laser_data.angle_increment = msg->angle_increment;
     //memcpy(scan_data, &(msg->ranges), sizeof(scan_data));
     count++;
     if (count % 10 == 0) {
-        ROS_INFO("%10f  %10f %10f %ld", msg->ranges[544], msg->ranges[msg->ranges.size()/2], msg->ranges[182], msg->ranges.size());
+        ROS_INFO("%10f  %10f %10f 10f 10f 10f %ld", msg->ranges[544], msg->ranges[msg->ranges.size()/2], msg->ranges[182],msg->min_angle, msg->max_angle, msg->angle_increment, msg->ranges.size());
         count = 0;
     }
 }
