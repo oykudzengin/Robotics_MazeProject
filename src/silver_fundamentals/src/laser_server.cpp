e6e460c6dc6b4a4058916341887d158ed9947570
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-04-25 15:05:01 +0200

    laser server test

--- src/silver_fundamentals/src/laser_server.cpp
+++ src/silver_fundamentals/src/laser_server.cpp
@@ -9,9 +9,9 @@
 
 static struct laser_data
 {
-    float ranges[726];
-    float range_min;
-    float range_max;
+    double ranges[726];
+    double range_min;
+    double range_max;
 } laser_data;
 
 static int count = 0;
@@ -32,7 +32,7 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
         int count = 0;
         for (int i = 0; i < 726; i++)
         {
-            if (!(laser_data.ranges[i] > laser_data.range_max || laser_data.ranges[i] < laser_data.range_min)) count ++;
+            if (laser_data.ranges[i] > laser_data.range_max || laser_data.ranges[i] < laser_data.range_min) count ++;
         }
         ROS_INFO("%10f %10f %10f %10f %10f %d", msg->ranges[544], msg->ranges[msg->ranges.size()/2], msg->ranges[0],msg->range_min, msg->range_max, count);
         count = 0;
