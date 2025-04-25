12208531cd32c8d756c2670d7cbba40f6323f070
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-04-25 22:59:25 +0200

    laser server test, might be broken, but now discarding correct values

--- src/silver_fundamentals/src/laser_server.cpp
+++ src/silver_fundamentals/src/laser_server.cpp
@@ -25,9 +25,9 @@ bool laser_angle_range(silver_fundamentals::Laser::Request  &req, silver_fundame
     double min_angle = req.start;
     double max_angle = req.end;
     int min_index = static_cast<int>(std::round(
-        std::max(min_angle + LASER_ANGLE_RANGE / 2, 0.0) * LASER_ARAY_SIZE / LASER_ANGLE_RANGE));
+        std::max(min_angle + LASER_ANGLE_RANGE / 2.0, 0.0) * LASER_ARAY_SIZE / LASER_ANGLE_RANGE));
     int max_index = static_cast<int>(std::round(
-        std::min(max_angle + LASER_ANGLE_RANGE / 2, 240.0) * LASER_ARAY_SIZE / LASER_ANGLE_RANGE));
+        std::min(max_angle + LASER_ANGLE_RANGE / 2.0, 240.0) * LASER_ARAY_SIZE / LASER_ANGLE_RANGE));
 
     res.values = std::vector<double>(laser_data.ranges.begin() + min_index, laser_data.ranges.begin() + std::min(max_index+1, LASER_ARAY_SIZE));
     res.size = max_index - min_index + 1;
