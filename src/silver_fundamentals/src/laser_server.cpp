93aed1ba3d4320f2e21f4e8a4be07539d03ec746
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-04-25 15:06:13 +0200

    laser server test

--- src/silver_fundamentals/src/laser_server.cpp
+++ src/silver_fundamentals/src/laser_server.cpp
@@ -34,7 +34,7 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
         {
             if (laser_data.ranges[i] > laser_data.range_max || laser_data.ranges[i] < laser_data.range_min) count ++;
         }
-        ROS_INFO("%10f %10f %10f %10f %10f %d", msg->ranges[544], msg->ranges[msg->ranges.size()/2], msg->ranges[0],msg->range_min, msg->range_max, count);
+        ROS_INFO("%10lf %10lf %10lf %10lf %10lf %d", msg->ranges[544], msg->ranges[msg->ranges.size()/2], msg->ranges[0],msg->range_min, msg->range_max, count);
         count = 0;
     }
 }
