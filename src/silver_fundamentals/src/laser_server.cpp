f0f0b43d054439d12d3b5bff6bfa91065ead3699
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-04-25 20:52:54 +0200

    laser server test, might be broken, but now discarding correct values

--- src/silver_fundamentals/src/laser_server.cpp
+++ src/silver_fundamentals/src/laser_server.cpp
@@ -2,27 +2,28 @@
 #include "silver_fundamentals/Laser.h"
 #include "sensor_msgs/LaserScan.h"
 #include <sstream>
-
-#include <cstring>
+#include <array>
 #include <bits/streambuf_iterator.h>
 
 static struct laser_data
 {
-    double ranges[682];
+    std::array<double, 682> ranges;
+    double angle_range = 0;
+    double angle_increment = 0;
     double range_min;
     double range_max;
 } laser_data;
 
 static int count = 0;
 
-bool complete_laser_data(silver_fundamentals::Laser::Request  &req,
+bool laser_angle_range(silver_fundamentals::Laser::Request  &req,
           silver_fundamentals::Laser::Response &res) {
             return false;
  }
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-    std::copy(msg->ranges.begin()+44, msg->ranges.end(), laser_data.ranges-44);
+    std::copy(msg->ranges.begin()+44, msg->ranges.end(), laser_data.ranges.begin());
     laser_data.range_min = msg->angle_min;
     laser_data.range_max = msg->angle_max;
 
@@ -30,7 +31,7 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
     count++;
     if (count % 10 == 0) {
         std::stringstream ss;
-        for (double range : laser_data.ranges)
+        for (const double range : laser_data.ranges)
         {
             ss << range << " ";
         }
@@ -44,7 +45,7 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "laser_server");
     ros::NodeHandle n;
 
-    ros::ServiceServer service = n.advertiseService("complete_laser_data", complete_laser_data);
+    ros::ServiceServer service = n.advertiseService("LaserAngleRange", laser_angle_range);
     ros::Subscriber sub = n.subscribe("scan_filtered", 1, laserCallback);
 
     ROS_INFO("Ready to serve");
