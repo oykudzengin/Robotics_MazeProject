717af45b8160c8a9f07e42be80b3f8dd2a9e318e
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-04-25 23:38:26 +0200

    laser server test, might be broken, but now discarding correct values

--- src/silver_fundamentals/src/wanderer.cpp
+++ src/silver_fundamentals/src/wanderer.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include "sensor_msgs/LaserScan.h"
 #include "create_fundamentals/DiffDrive.h"
+#include "silver_fundamentals/Laser.h"
 #include <cmath>
 #include <sstream>
 #include <config.h>
