0a4514d193ac70b0beb78da02b988220fc0f8ee7
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-04-25 22:42:10 +0200

    laser server test, might be broken, but now discarding correct values

--- src/silver_fundamentals/src/laser_server.cpp
+++ src/silver_fundamentals/src/laser_server.cpp
@@ -13,7 +13,7 @@
 
 static struct laser_data
 {
-    std::array<double, LASER_ARAY_SIZE> ranges;
+    std::array<double, static_cast<int>(LASER_ARAY_SIZE)> ranges;
     double angle_range = 4.178563637658954*180/PI;
     double angle_min = -2.086213869974017*180/PI;
     double angle_increment = 0.006135923322290182*180/PI;
@@ -22,15 +22,15 @@ static struct laser_data
 static int count = 0;
 
 bool laser_angle_range(silver_fundamentals::Laser::Request  &req, silver_fundamentals::Laser::Response &res) {
-    double min_angle = req->start;
-    double max_angle = req->end;
+    double min_angle = req.start;
+    double max_angle = req.end;
     int min_index = static_cast<int>(std::round(
         std::max(min_angle + LASER_ANGLE_RANGE / 2, 0.0) * LASER_ARAY_SIZE / LASER_ANGLE_RANGE));
     int max_index = static_cast<int>(std::round(
         std::min(max_angle + LASER_ANGLE_RANGE / 2, 240.0) * LASER_ARAY_SIZE / LASER_ANGLE_RANGE));
 
-    res->values = std::vector<double>(laser_data.ranges.begin() + min_index, laser_data.ranges.begin() + std::min(max_index+1, LASER_ARAY_SIZE));
-    res->size = max_index - min_index + 1;
+    res.values = std::vector<double>(laser_data.ranges.begin() + min_index, laser_data.ranges.begin() + std::min(max_index+1, LASER_ARAY_SIZE));
+    res.size = max_index - min_index + 1;
     return true;
  }
 
