c68519c40d78c43ad7d5e492d8f8864f80ed65eb
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-04-29 15:08:42 +0200

    .fixing

--- src/silver_fundamentals/src/laser_server.cpp
+++ src/silver_fundamentals/src/laser_server.cpp
@@ -27,7 +27,7 @@ int minimum_discarding(std::vector<double> *ranges)
     int count = 0;
     for (int i = 0; i < static_cast<int>(ranges->size()); i++)
     {
-        if (ranges->at(i) < laser_data.ranges.at(i))
+        if (ranges->at(i) < laser_data.range_min)
         {
             ranges->at(i) = std::numeric_limits<double>::signaling_NaN();
             count++;
