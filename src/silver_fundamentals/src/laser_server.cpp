3d4e8e8b0cc11c84e2c7130d5297b23bb041bfa2
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-04-30 20:11:42 +0200

    debugging

--- src/silver_fundamentals/src/laser_server.cpp
+++ src/silver_fundamentals/src/laser_server.cpp
@@ -44,7 +44,7 @@ bool laser_angle_range(silver_fundamentals::Laser::Request  &req, silver_fundame
         std::max(min_angle + LASER_ANGLE_RANGE / 2.0, 0.0) * LASER_ARAY_SIZE / LASER_ANGLE_RANGE));
     int max_index = static_cast<int>(std::round(
         std::min(max_angle + LASER_ANGLE_RANGE / 2.0, 240.0) * LASER_ARAY_SIZE / LASER_ANGLE_RANGE));
-    ROS_INFO("Creating array between %d and %d", min_index, max_index);
+    //ROS_INFO("Creating array between %d and %d", min_index, max_index);
 
     res.values = std::vector<double>(laser_data.ranges.begin() + min_index, laser_data.ranges.begin() + std::min(max_index+1, static_cast<int>(LASER_ARAY_SIZE)));
     minimum_discarding(&res.values);
