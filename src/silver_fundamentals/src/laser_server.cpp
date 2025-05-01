ac234dd70edf100772f8d9e6eb6bef3bf458bd24
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-05-01 13:31:13 +0200

    formatting

--- src/silver_fundamentals/src/laser_server.cpp
+++ src/silver_fundamentals/src/laser_server.cpp
@@ -1,12 +1,12 @@
-#include "ros/ros.h"
-#include "silver_fundamentals/Laser.h"
-#include "sensor_msgs/LaserScan.h"
-#include <sstream>
 #include <array>
-#include <vector>
-#include <cmath>
 #include <bits/streambuf_iterator.h>
+#include <cmath>
 #include <config.h>
+#include <sstream>
+#include <vector>
+#include "ros/ros.h"
+#include "sensor_msgs/LaserScan.h"
+#include "silver_fundamentals/Laser.h"
 
 #define LASER_ARAY_SIZE 682.0
 #define LASER_ANGLE_RANGE 240.0
@@ -14,15 +14,15 @@
 static struct laser_data
 {
     std::array<double, static_cast<int>(LASER_ARAY_SIZE)> ranges;
-    double angle_range = 4.178563637658954*180/PI;
-    double angle_min = -2.086213869974017*180/PI;
-    double angle_increment = 0.006135923322290182*180/PI;
+    double angle_range = 4.178563637658954 * 180 / PI;
+    double angle_min = -2.086213869974017 * 180 / PI;
+    double angle_increment = 0.006135923322290182 * 180 / PI;
     double range_min = 0.019999999552965164;
 } laser_data;
 
 static int count = 0;
 
-int minimum_discarding(std::vector<double> *ranges)
+int minimum_discarding(std::vector<double>* ranges)
 {
     int count = 0;
     for (int i = 0; i < static_cast<int>(ranges->size()); i++)
@@ -37,27 +37,31 @@ int minimum_discarding(std::vector<double> *ranges)
 }
 
 
-bool laser_angle_range(silver_fundamentals::Laser::Request  &req, silver_fundamentals::Laser::Response &res) {
+bool laser_angle_range(silver_fundamentals::Laser::Request& req, silver_fundamentals::Laser::Response& res)
+{
     double min_angle = req.start;
     double max_angle = req.end;
-    int min_index = static_cast<int>(std::round(
-        std::max(min_angle + LASER_ANGLE_RANGE / 2.0, 0.0) * LASER_ARAY_SIZE / LASER_ANGLE_RANGE));
-    int max_index = static_cast<int>(std::round(
-        std::min(max_angle + LASER_ANGLE_RANGE / 2.0, 240.0) * LASER_ARAY_SIZE / LASER_ANGLE_RANGE));
-    //ROS_INFO("Creating array between %d and %d", min_index, max_index);
+    int min_index = static_cast<int>(
+        std::round(std::max(min_angle + LASER_ANGLE_RANGE / 2.0, 0.0) * LASER_ARAY_SIZE / LASER_ANGLE_RANGE));
+    int max_index = static_cast<int>(
+        std::round(std::min(max_angle + LASER_ANGLE_RANGE / 2.0, 240.0) * LASER_ARAY_SIZE / LASER_ANGLE_RANGE));
+    // ROS_INFO("Creating array between %d and %d", min_index, max_index);
 
-    res.values = std::vector<double>(laser_data.ranges.begin() + min_index, laser_data.ranges.begin() + std::min(max_index+1, static_cast<int>(LASER_ARAY_SIZE)));
+    res.values =
+        std::vector<double>(laser_data.ranges.begin() + min_index,
+                            laser_data.ranges.begin() + std::min(max_index + 1, static_cast<int>(LASER_ARAY_SIZE)));
     minimum_discarding(&res.values);
     res.size = max_index - min_index + 1;
     return true;
- }
+}
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-    std::copy(msg->ranges.begin()+44, msg->ranges.end(), laser_data.ranges.begin());
+    std::copy(msg->ranges.begin() + 44, msg->ranges.end(), laser_data.ranges.begin());
 
     count++;
-    if (count % 100 == 0) {
+    if (count % 100 == 0)
+    {
         std::stringstream ss;
         for (const double range : laser_data.ranges)
         {
@@ -68,7 +72,7 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
     }
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     ros::init(argc, argv, "laser_server");
     ros::NodeHandle n;
