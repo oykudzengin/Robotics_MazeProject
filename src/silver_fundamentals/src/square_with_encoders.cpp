5d97f291cfe93e32174e85c19e37c6009c1cdf74
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-05-02 18:46:39 +0200

    square done? test 2

--- src/silver_fundamentals/src/square_with_encoders.cpp
+++ src/silver_fundamentals/src/square_with_encoders.cpp
@@ -7,6 +7,8 @@ int main(int argc, char **argv) {
     double wheel_radius = std::atof(argv[1]);
     double wheel_base = std::atof(argv[2]);
     ros::init(argc, argv, "square_with_encoders");
+    // larger wheelbase -> higher turning angle
+    // larger wheelradius -> going less far
     FeedbackDrive driver = FeedbackDrive(wheel_radius, wheel_base, 10.0);
     driver.reset_encoders();
     for (int i = 0; i < 4; i++) {
