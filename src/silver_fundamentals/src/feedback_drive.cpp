098537be4f308ca55700fbc8c69342f6b706f56e
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-05-02 16:51:28 +0200

    add to cmake 08

--- src/silver_fundamentals/src/feedback_drive.cpp
+++ src/silver_fundamentals/src/feedback_drive.cpp
@@ -14,6 +14,7 @@ void FeedbackDrive::drive_n_cm(double n) {
     double distance_in_rad = n/wheel_radius;
 
     reset_encoders_client.call(reset_encoders_srv);
+    ROS_INFO("%d, %d", ros::ok(), drive_data_client.call(drive_data_srv));
     while (ros::ok() && drive_data_client.call(drive_data_srv)) {
         double current_rad_distance = (drive_data_srv.response.left_encoder + drive_data_srv.response.right_encoder)/2.0;
         if (current_rad_distance > distance_in_rad)
