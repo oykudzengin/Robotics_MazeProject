b90595c3d04cb25fbcdf2a6464b02378b3514615
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-05-02 18:40:24 +0200

    square done? test 1

--- src/silver_fundamentals/src/square_with_encoders.cpp
+++ src/silver_fundamentals/src/square_with_encoders.cpp
@@ -1,10 +1,13 @@
 #include "ros/ros.h"
 #include <feedback_drive.h>
+#include <string.h>
 
 
 int main(int argc, char **argv) {
+    double wheel_radius = std::atof(argv[1]);
+    double wheel_base = std::atof(argv[2]);
     ros::init(argc, argv, "square_with_encoders");
-    FeedbackDrive driver = FeedbackDrive(3.2, 24.8, 10.0);
+    FeedbackDrive driver = FeedbackDrive(wheel_radius, wheel_base, 10.0);
     driver.reset_encoders();
     for (int i = 0; i < 4; i++) {
         driver.drive_n_cm(100);
