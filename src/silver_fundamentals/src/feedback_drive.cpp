27fbf39fe5d4f3dfd8141964cffd251707be9962
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-05-02 17:36:52 +0200

    square done? 08

--- src/silver_fundamentals/src/feedback_drive.cpp
+++ src/silver_fundamentals/src/feedback_drive.cpp
@@ -19,7 +19,7 @@ void FeedbackDrive::drive_n_cm(double n) {
     drive_srv.request.right = speed;
     double distance_in_rad = n/wheel_radius;
 
-    if (reset_encoders_client.call(reset_encoders_srv));
+    while (!reset_encoders_client.call(reset_encoders_srv));
     drive_client.call(drive_srv);
     ROS_INFO("%d, %d", ros::ok(), drive_data_client.call(drive_data_srv));
     while (ros::ok() && drive_data_client.call(drive_data_srv)) {
@@ -60,7 +60,7 @@ void FeedbackDrive::turn_n_degrees(double n, direction d) {
     }
     double distance_in_rad = (n*wheel_base*PI)/(360.0*wheel_radius);
 
-    if (reset_encoders_client.call(reset_encoders_srv));
+    while (!reset_encoders_client.call(reset_encoders_srv));
     drive_client.call(drive_srv);
     while (ros::ok() && drive_data_client.call(drive_data_srv)) {
         double current_rad_distance = (std::abs(drive_data_srv.response.left_encoder) + std::abs(drive_data_srv.response.right_encoder))/2.0;
