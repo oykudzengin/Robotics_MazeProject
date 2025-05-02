ef47ca2f4105cdae1dc0202e4e4b352fc8d98ba4
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-05-02 17:25:17 +0200

    square done? 04

--- src/silver_fundamentals/src/square_with_encoders.cpp
+++ src/silver_fundamentals/src/square_with_encoders.cpp
@@ -5,8 +5,5 @@
 int main(int argc, char **argv) {
     ros::init(argc, argv, "square_with_encoders");
     FeedbackDrive driver = FeedbackDrive(3.2, 25.0, 10.0);
-    for (int i = 0; i < 4; i++) {
-        driver.drive_n_cm(100);
-        driver.turn_n_degrees(90, right);
-    }
+    driver.turn_n_degrees(90, right);
 }
