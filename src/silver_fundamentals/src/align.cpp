e548a87ed7a17770f87b9fd7704234b99f95002e
maximilian <maximilian.kirst@tu-berlin.de>
2025-05-04 12:48:22 +0200

    ransac 10

--- src/silver_fundamentals/src/align.cpp
+++ src/silver_fundamentals/src/align.cpp
@@ -264,8 +264,8 @@ int align() {
 int main(int argc, char **argv) {
     ros::init(argc, argv, "align");
 
-    align();
-    return 0;
+    //align();
+    //return 0;
 
     ros::NodeHandle n;
 
@@ -278,36 +278,7 @@ int main(int argc, char **argv) {
     double max_offset = std::atof(argv[1]);
     double iter = std::atof(argv[2]);
 
-    /*
-    while (ros::ok()) {
-        laser_cart_srv.request.start = -120;
-        laser_cart_srv.request.end = 120;
 
-        if (!laser_cart_client.call(laser_cart_srv)) {
-            ROS_ERROR("Failed to call laser_cart_client.call()");
-            continue;
-        }
-
-        // print out the points
-        const auto& pts = laser_cart_srv.response.values;
-        std::ostringstream oss;
-        oss << "Points: ";
-        for (size_t i = 0; i < pts.size(); ++i) {
-            const auto& p = pts[i];
-            oss << "("
-                << std::fixed << std::setprecision(3)
-                << p.x << "," << p.y
-                << ")";
-            if (i + 1 < pts.size()) oss << ", ";
-        }
-
-        // Print them all in one line:
-        ROS_INFO("%s", oss.str().c_str());
-        ROS_INFO("Total points returned: %d", laser_cart_srv.response.size);
-
-        rate.sleep();
-    }
-    */
     while (ros::ok()) {
         laser_cart_srv.request.start = -120;
         laser_cart_srv.request.end = 120;
