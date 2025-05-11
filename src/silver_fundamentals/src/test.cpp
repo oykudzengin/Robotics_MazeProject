22d11b3aa7f538f3600c01719dbcb77344ec557e
maximilian <maximilian.kirst@tu-berlin.de>
2025-05-11 16:13:30 +0200

    test files

---
+++ src/silver_fundamentals/src/test.cpp
@@ -0,0 +1,24 @@
+
+#include "ros/ros.h"
+#include <feedback_drive.h>
+#include <string.h>
+
+// wheel diameter 3.25
+// wheelbase 26.203
+
+int main(int argc, char **argv) {
+    int amount = 1;
+    if (argc >= 2) {
+        amount = std::atoi(argv[1]);
+    }
+    ros::init(argc, argv, "square_with_encoders");
+    // larger wheelbase -> higher turning angle
+    // larger wheelradius -> going further
+    FeedbackDrive driver = FeedbackDrive(3.25, 23.5, 10.0);
+    driver.reset_encoders();
+    for (int i = 0; i < 4*amount; i++) {
+        driver.drive_n_cm(100);
+        driver.turn_n_degrees(90.0, right);
+    }
+
+}
