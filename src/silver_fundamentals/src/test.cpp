1e4363a44b2bc7d0fbb3e4556431f00d6a5f9938
maximilian <maximilian.kirst@tu-berlin.de>
2025-05-11 16:24:52 +0200

    test files update

--- src/silver_fundamentals/src/test.cpp
+++ src/silver_fundamentals/src/test.cpp
@@ -7,18 +7,25 @@
 // wheelbase 26.203
 
 int main(int argc, char **argv) {
-    int amount = 1;
-    if (argc >= 2) {
-        amount = std::atoi(argv[1]);
+    double speed;
+    direction dir;
+    if (argc <= 4) {
+        dir = right;
+    } else {
+        dir = atoi(argv[4]) == -1 ? right: (atoi(argv[4]) == 1 ? left : none);
     }
-    ros::init(argc, argv, "square_with_encoders");
+    if (argc <= 3) {
+        speed = 4.0;
+    } else {
+        speed = atof(argv[3]);
+    }
+    double angle = atof(argv[1]);
+    double radius = atof(argv[2]);
+    ros::init(argc, argv, "test");
     // larger wheelbase -> higher turning angle
     // larger wheelradius -> going further
     FeedbackDrive driver = FeedbackDrive(3.25, 23.5, 10.0);
     driver.reset_encoders();
-    for (int i = 0; i < 4*amount; i++) {
-        driver.drive_n_cm(100);
-        driver.turn_n_degrees(90.0, right);
-    }
+    driver.turn(angle, dir, radius, speed);
 
 }
