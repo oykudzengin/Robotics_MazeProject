0c43fa6ecbdab963289dbd66ee79b54c01c46229
maximilian <maximilian.kirst@tu-berlin.de>
2025-05-14 18:05:20 +0200

    test combined wall dist

--- src/silver_fundamentals/src/feedback_drive.cpp
+++ src/silver_fundamentals/src/feedback_drive.cpp
@@ -299,13 +299,13 @@ int FeedbackDrive::drive_along_wall(const double right_wall_dist, const double l
 
     int ransac_iterations = 7000;
     double ransac_distance = 0.8;
-    double used_values_proportion = 0.8;
+    double used_values_proportion = 0.95;
 
-    right_laser_srv.request.start = -110.0;
-    right_laser_srv.request.end = -70.0;
+    right_laser_srv.request.start = -100.0;
+    right_laser_srv.request.end = -80.0;
     right_laser_srv.request.max_dist = 100.0;
-    left_laser_srv.request.start = 70.0;
-    left_laser_srv.request.end = 110.0;
+    left_laser_srv.request.start = 80.0;
+    left_laser_srv.request.end = 100.0;
     left_laser_srv.request.max_dist = 100.0;
     front_laser_srv.request.start = -25.0;
     front_laser_srv.request.end = 25.0;
