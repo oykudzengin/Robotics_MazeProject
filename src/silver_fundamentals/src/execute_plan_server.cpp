42a4e3cfd6abd52ee7e52a7d2553643d2459fba9
silver <silver@robot.com>
2025-05-14 14:19:22 +0000

    exec plan 3

--- src/silver_fundamentals/src/execute_plan_server.cpp
+++ src/silver_fundamentals/src/execute_plan_server.cpp
@@ -1,7 +1,7 @@
 #include <ros/ros.h>
 #include <silver_fundamentals/ExecutePlan.h>
 #include "silver_fundamentals/Laser.h"
-#include "feedback_drive.h"
+#include <feedback_drive.h>
 
 bool executePlan(silver_fundamentals::ExecutePlan::Request &req,
                   silver_fundamentals::ExecutePlan::Response &res) 
@@ -11,31 +11,37 @@ bool executePlan(silver_fundamentals::ExecutePlan::Request &req,
     auto driver = FeedbackDrive(3.25, 26.203, 2.0);
 
     std::vector<int> plan = req.plan;
+    ros::Rate loop_rate(10);
 
-    for (int i=0; i<plan.size(); i++) {
+    for (int i=0; i < plan.size(); i++) {
         switch (plan[i]) {
             case 0: { //right
+                ROS_INFO("Turning right");
                 driver.turn_n_degrees(90, right);
                 driver.drive_n_cm(40);
                 break;
             }
             case 1: { //up
+                ROS_INFO("Moving up");
                 driver.drive_n_cm(40);
                 break;
             }
             case 2: { //left
-                driver.turn__n_degrees(90,left);
-                driver.drive_n_cm(10);
+                ROS_INFO("Turning left");
+                driver.turn_n_degrees(90,left);
+                driver.drive_n_cm(40);
                 break;
             }
             case 3: { //down
+                ROS_INFO("Moving down");
                 driver.turn_n_degrees(180, right);
                 driver.drive_n_cm(40);
                 break;
             }
             default:
-                ROS_ERROR("Unknown action %d", action);
+                ROS_ERROR("Unknown action %d", plan[i]);
         }
+        loop_rate.sleep();
     }
 
     return true;
