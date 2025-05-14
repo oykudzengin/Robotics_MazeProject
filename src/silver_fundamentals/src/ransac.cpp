946624bbb2a410df1c1a1b731a94db0efb87f816
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-05-14 19:50:32 +0200

    ransac2

--- src/silver_fundamentals/src/ransac.cpp
+++ src/silver_fundamentals/src/ransac.cpp
@@ -86,8 +86,70 @@ double ransac_with_dist(std::vector<geometry_msgs::Point> &pts, const double max
     return -angle_deg;
 }
 
+
+
+double new_ransac(std::vector<geometry_msgs::Point> &pts, const double max_offset, const int max_iterations, int *inliers_used, double *dist) {
+    if (pts.size() < 2) {
+        *inliers_used = 0;
+        *dist   = std::numeric_limits<double>::infinity();
+        return std::numeric_limits<double>::infinity();
+    }
+
+    std::mt19937 rng(std::random_device{}());
+    std::uniform_int_distribution<unsigned int> uni(0, pts.size() - 1);
+
+    unsigned int best_count = 0;
+    double best_a = 0, best_b = 0, best_c = 0;
+
+    for (int i = 0; i < max_iterations; i++) {
+        const unsigned int first_idx = uni(rng);
+        const unsigned int second_idx = uni(rng);
+
+        if (first_idx == second_idx) {
+            continue;
+        }
+
+        auto &P = pts[first_idx], &Q = pts[second_idx];
+        double a = Q.y - P.y;
+        double b = P.x - Q.x;
+        double c = -a*P.x + b*P.y;
+
+        unsigned int inliers = 0;
+        for (const auto &pt: pts) {
+            const double distance_to_line = std::fabs((a * pt.x + b * pt.y + c)) / (std::sqrt(a * a + b * b));
+            if (distance_to_line <= max_offset) {
+                inliers++;
+            }
+        }
+        if (inliers > best_count) {
+            best_count = inliers;
+            best_a = a;
+            best_b = b;
+            best_c = c;
+        }
+    }
+
+    if (best_count < 2) {
+        *inliers_used = 0;
+        *dist   = std::numeric_limits<double>::infinity();
+        return std::numeric_limits<double>::infinity();
+    }
+
+    pts.erase(std::remove_if(pts.begin(), pts.end(), [&](const geometry_msgs::Point &pt) {
+        return std::fabs((best_a * pt.x + best_b * pt.y + best_c)) / std::sqrt(best_a * best_a + best_b * best_b) <= max_offset;
+    }), pts.end());
+
+    double theta_rad = std::atan2(best_a < 0? -best_a:best_a, best_a < 0? best_b : -best_b);
+    double theta_deg = theta_rad * 180.0 / PI;
+
+    *dist = std::fabs(best_c/std::sqrt(best_a * best_a + best_b * best_b));
+
+    return theta_deg;
+
+}
+
 double ransac(std::vector<geometry_msgs::Point> &pts, const double max_offset, const int max_iterations, int *amount) {
     double unused;
-    return ransac_with_dist(pts, max_offset, max_iterations, amount, &unused);
+    return new_ransac(pts, max_offset, max_iterations, amount, &unused);
 }
 
