db5193f6e78c4f214ed6027547b20b07a2e47de3
maximilian <maximilian.kirst@tu-berlin.de>
2025-05-15 19:28:19 +0200

    potential drive? 14

--- src/silver_fundamentals/src/feedback_drive.cpp
+++ src/silver_fundamentals/src/feedback_drive.cpp
@@ -416,7 +416,7 @@ int FeedbackDrive::potential_field_drive(geometry_msgs::Point goal, double k_att
     double base_line_left = encoder_srv.response.left_encoder;
     double base_line_right = encoder_srv.response.right_encoder;
 
-    double currrent_encoder_right = base_line_right;
+    double current_encoder_right = base_line_right;
     double current_encoder_left = base_line_left;
 
     const double base_speed = 4.0;
@@ -427,8 +427,8 @@ int FeedbackDrive::potential_field_drive(geometry_msgs::Point goal, double k_att
     current_pos.y = 0.0;
     current_pos.z = 0.0;
     do {
-        geometry_msgs::Point field_vector = get_potentials(current_pos, goal, k_att, k_rep, r);
-        double angle = std::atan2(field_vector.x, field_vector.y);
+        const geometry_msgs::Point field_vector = get_potentials(current_pos, goal, k_att, k_rep, r);
+        const double angle = std::atan2(field_vector.x, field_vector.y);
 
         ROS_INFO("Field vector x is %f, y is %f, angle is %f", field_vector.x, field_vector.y, angle);
         drive_srv.request.left = base_speed - (angle/PI)*8;
@@ -445,6 +445,6 @@ int FeedbackDrive::potential_field_drive(geometry_msgs::Point goal, double k_att
 
         current_pos = position_update(current_pos, current_encoder_right-base_line_right, current_encoder_left-base_line_left);
 
-    } while (ros::ok() && std::sqrt((current_pos.x-goal.x) * (current_pos.x-goal.x) + (current_pos.y-goal.y) * (current_pos.y-goal.y) > 0.1);
+    } while (ros::ok() && std::sqrt((current_pos.x-goal.x) * (current_pos.x-goal.x) + (current_pos.y-goal.y) * (current_pos.y-goal.y)) > 0.1);
 }
 
