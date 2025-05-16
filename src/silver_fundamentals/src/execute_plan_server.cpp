26a149667bb53c088e6fb7256ccb522b06c18e7b
tim <timnicknacken@gmail.com>
2025-05-16 19:43:48 +0200

    update fucntion signature to use vector and not array

--- src/silver_fundamentals/src/execute_plan_server.cpp
+++ src/silver_fundamentals/src/execute_plan_server.cpp
@@ -8,32 +8,21 @@
 // Example usage
 #include <iostream>
 
-// typedef struct  movement
-// {
-//     direction: dir;
-//     union {
-//         double radius;
-//         double distance;
-//     } dist;
-// } movement;
 
-//movement.dist.radius;
-//movement.dist.distance;
-std::vector<int> compressPlan(const int plan[], std::size_t length) {
-
-    // 1. Build an globalPlan with a leading '1'
-    std::vector<int> globalPlan;
-    globalPlan.reserve(length + 1);
-    globalPlan.push_back(1);  // initial heading for conversion
-    for (std::size_t i = 0; i < length; ++i) {
-        globalPlan.push_back(plan[i]);
+std::vector<int> compressPlan(const std::vector<int32_t>& globalPlan) {
+    // 1. Build an extPlan with a leading '1'
+    std::vector<int> extPlan;
+    extPlan.reserve(globalPlan.size() + 1);
+    extPlan.push_back(1);  // initial heading for conversion
+    for (auto dir : globalPlan) {
+        extPlan.push_back(static_cast<int>(dir));
     }
 
     // 2. Compute local instructions
     std::vector<int> localPlan;
-    localPlan.reserve(globalPlan.size() - 1);
-    for (std::size_t i = 0; i + 1 < globalPlan.size(); ++i) {
-        int diff = (globalPlan[i] - globalPlan[i+1] + 4) % 4;
+    localPlan.reserve(extPlan.size() - 1);
+    for (size_t i = 0; i + 1 < extPlan.size(); ++i) {
+        int diff = (extPlan[i] - extPlan[i+1] + 4) % 4;
         localPlan.push_back(diff);
     }
 
@@ -88,15 +77,6 @@ int main(int argc, char **argv)
     ros::ServiceServer service = n.advertiseService("execute_plan", executePlan);
     ROS_INFO("Ready to execute plan.");
     ros::spin();
-
-    // int plan[] = {3, 0, 3, 2, 2, 1};
-    // auto local = compressPlan(plan, sizeof(plan) / sizeof(plan[0]));
-
-    // std::cout << "Local plan: ";
-    // for (int step : local) {
-    //     std::cout << step << ' ';
-    // }
-    // std::cout << std::endl;
 
     return 0;
 }
