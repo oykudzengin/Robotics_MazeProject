d90f9c612dce0d07391768c47c508eb4eac84b30
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-05-16 21:36:15 +0200

    potential drive clip turning angles 2

--- src/silver_fundamentals/src/feedback_drive.cpp
+++ src/silver_fundamentals/src/feedback_drive.cpp
@@ -429,7 +429,7 @@ geometry_msgs::Point FeedbackDrive::get_potentials(geometry_msgs::Point current_
 
 int FeedbackDrive::potential_field_drive(geometry_msgs::Point goal, double k_att, double k_rep, double r, double rot_rate) {
     double min_turning_angle = 4.0/180.0*PI;
-    double max_turning_angle = 180.0/180.0*PI;
+    double max_turning_angle = 30.0/180.0*PI;
 
 
 	auto sleep_rate = ros::Rate(100);
@@ -442,7 +442,7 @@ int FeedbackDrive::potential_field_drive(geometry_msgs::Point goal, double k_att
     double current_encoder_right = base_line_right;
     double current_encoder_left = base_line_left;
 
-    const double base_speed = 3.0;
+    const double base_speed = 8.0;
 
 
     geometry_msgs::Point current_pos;
