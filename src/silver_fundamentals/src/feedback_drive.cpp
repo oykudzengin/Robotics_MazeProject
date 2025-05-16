c64c47bf259425e51658361f089312b0cc3c375a
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-05-16 20:21:03 +0200

    potential drive? 42

--- src/silver_fundamentals/src/feedback_drive.cpp
+++ src/silver_fundamentals/src/feedback_drive.cpp
@@ -11,6 +11,7 @@
 #include <ransac.h>
 
 #define ROBOT_RADIUS 13.3f
+#define LIDAR_SENSOR_OFFSET 14.0f
 
 #define WIDTH_THRESHOLD_RADIUS 4f
 #define ANGLE_THRESHOLD 5f
@@ -410,7 +411,7 @@ geometry_msgs::Point FeedbackDrive::get_potentials(geometry_msgs::Point current_
 
     for (auto &pt : laser_srv.response.values) {
         double real_x = pt.y;
-        double real_y = pt.x;
+        double real_y = pt.x - LIDAR_SENSOR_OFFSET;
         double d_zero = std::sqrt(real_x * real_x + real_y * real_y);
         if (d_zero > r)
             continue;
@@ -448,7 +449,7 @@ int FeedbackDrive::potential_field_drive(geometry_msgs::Point goal, double k_att
         const geometry_msgs::Point field_vector = get_potentials(current_pos, goal, k_att, k_rep, r);
         const double angle = std::atan2(field_vector.x, field_vector.y);
 
-        ROS_INFO("Field vector x is %f, y is %f, angle is %f", field_vector.x, field_vector.y, angle/PI*180.0);
+        ROS_INFO("Field vector x is %f, y is %f, angle is %f Current pos is %f %f %f", field_vector.x, field_vector.y, angle/PI*180.0, current_pos.x, current_pos.y, current_pos.z/PI*180.);
         double rotation_rate = angle * rot_rate;
         drive_srv.request.left = base_speed - wheel_base/2 * rotation_rate;
         drive_srv.request.right = base_speed + wheel_base/2 * rotation_rate;
@@ -463,7 +464,7 @@ int FeedbackDrive::potential_field_drive(geometry_msgs::Point goal, double k_att
         current_encoder_right = encoder_srv.response.right_encoder;
 
         current_pos = position_update(current_pos, current_encoder_right-base_line_right, current_encoder_left-base_line_left);
-        ROS_INFO("Current pos is %f %f %f", current_pos.x, current_pos.y, current_pos.z/PI*180.0);
+        //ROS_INFO("Current pos is %f %f %f", current_pos.x, current_pos.y, current_pos.z/PI*180.0);
 
     } while (ros::ok() && std::sqrt((current_pos.x-goal.x) * (current_pos.x-goal.x) + (current_pos.y-goal.y) * (current_pos.y-goal.y)) > 0.1);
 
