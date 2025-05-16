b9a92aa9e6586e0bd2f36b3abb3c25ce023775e1
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-05-16 15:20:41 +0200

    potential drive? 30

--- src/silver_fundamentals/src/feedback_drive.cpp
+++ src/silver_fundamentals/src/feedback_drive.cpp
@@ -400,12 +400,12 @@ geometry_msgs::Point FeedbackDrive::get_potentials(geometry_msgs::Point current_
     double c = std::cos(yaw);
     double s = std::sin(yaw);
 
-    double local_dx =  c*wx + s*wy;
-    double local_dy = s*wx - c*wy;
+    double local_dx =  c*wx - s*wy;
+    double local_dy = s*wx + c*wy;
 
 
-    double x_force = -k_att*local_dx;
-    double y_force = -k_att*local_dy;
+    double x_force = k_att*local_dx;
+    double y_force = k_att*local_dy;
     // ROS_INFO("x_force: %f current_x: %f goal_x: %f", x_force, current_pos.x, goal.x);
 
     for (auto &pt : laser_srv.response.values) {
