d87fe0141b3568f10d63d9ea045ded1e8d294584
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-05-16 12:23:59 +0200

    potential drive? 22

--- src/silver_fundamentals/include/feedback_drive.h
+++ src/silver_fundamentals/include/feedback_drive.h
@@ -20,7 +20,7 @@ class FeedbackDrive {
         int turn(double, direction, double, double);
         int drive_along_wall(double, double, double, bool (*)());
 
-        int potential_field_drive(geometry_msgs::Point goal, double k_att, double k_rep, double r);
+        int potential_field_drive(geometry_msgs::Point goal, double k_att, double k_rep, double r, double rot_rate);
 
 
     private:
