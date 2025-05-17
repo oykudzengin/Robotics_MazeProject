a4ab483dcca6842077e3d9ab172298a8ac5d8cb0
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-05-17 15:44:41 +0200

    fix drive into wall maybe and norm attraction 11

--- src/silver_fundamentals/src/feedback_drive.cpp
+++ src/silver_fundamentals/src/feedback_drive.cpp
@@ -472,11 +472,16 @@ int FeedbackDrive::potential_field_drive(geometry_msgs::Point goal, double k_att
         // clip angle to always be smaller max_turning_angle and ignore it if smaller min_turning_angle
         if (std::abs(angle) < min_turning_angle)
             angle = 0.0;
+
+        /*
         else if (angle-current_turning_angle > max_turning_angle)
             angle = current_turning_angle + max_turning_angle;
         else if (current_turning_angle-angle > max_turning_angle)
             angle = current_turning_angle - max_turning_angle;
         current_turning_angle = angle;
+         */
+        if (angle > 170.0)
+            angle -= 360.0;
 
         ROS_INFO("Field vector x is %f, y is %f, angle is %f Current pos is %f %f %f", field_vector.x, field_vector.y, angle/PI*180.0, current_pos.x, current_pos.y, current_pos.z/PI*180.);
         double rotation_rate = angle * rot_rate * base_speed;
