62a81b787ade1ec4d54bdbbeae62bcc4c50807d7
maximilian <maximilian.kirst@campus.tu-berlin.de>
2025-05-17 16:44:16 +0200

    first test

--- src/silver_fundamentals/src/execute_plan_server.cpp
+++ src/silver_fundamentals/src/execute_plan_server.cpp
@@ -79,42 +79,11 @@ bool executePlan(silver_fundamentals::ExecutePlan::Request &req,
 
     ROS_INFO("Received a plan of %d steps", req.plan.size());
     auto driver = FeedbackDrive(3.25, 26.203, 2.0);
+    driver.reset_encoders();
 
     std::vector<int> plan = req.plan; //compressPlan will work here
-    ros::Rate loop_rate(10);
-
-    for (int i=0; i < plan.size(); i++) {
-        locDirection cur_dir = static_cast<locDirection>(plan[i]); // Convert to locDirection enum
-        switch (cur_dir) {
-            case l_RIGHT: { //right
-                ROS_INFO("Turning right");
-                driver.turn_n_degrees(90, right);
-                driver.drive_n_cm(40);
-                break;
-            }
-            case l_UP: { //up
-                ROS_INFO("Moving up");
-                driver.drive_n_cm(40);
-                break;
-            }
-            case l_LEFT: { //left
-                ROS_INFO("Turning left");
-                driver.turn_n_degrees(90,left);
-                driver.drive_n_cm(40);
-                break;
-            }
-            case l_DOWN: { //down
-                ROS_INFO("Moving down");
-                driver.turn_n_degrees(180, right);
-                driver.drive_n_cm(40);
-                break;
-            }
-            default:
-                ROS_ERROR("Unknown action %d", plan[i]);
-        }
-        loop_rate.sleep();
-    }
-
+    auto waypoints = convertMovesToWaypoints(plan);
+    driver.feedback_drive(waypoints, 10.0, 0.01, 0.3, 0.08);
     return true;
 }
 
