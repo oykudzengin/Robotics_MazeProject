e26b1a2b30eb0754287d1c89008dbdb530cc2e29
maximilian <maximilian.kirst@tu-berlin.de>
2025-06-04 17:04:09 +0200

    resample maybe

--- src/silver_fundamentals/src/localize.cpp
+++ src/silver_fundamentals/src/localize.cpp
@@ -19,13 +19,29 @@
 #define AMOUNT_RANDOM_INJECTIONS 0
 #define PROBABILITY_RANDOM_INJECTIONS 0
 
+static std::random_device rd;
+static std::mt19937 gen(rd());
+
 struct Particle {
     geometry_msgs::Point position;
     double weight;
+
+    static Particle random(const LikelihoodField &lhf) {
+        static std::uniform_real_distribution<double> ux(0, lhf.get_col_count()*lhf.get_cell_size()/100.0);
+        static std::uniform_real_distribution<double> uy(0, lhf.get_row_count()*lhf.get_cell_size()/100.0);
+        static std::uniform_real_distribution<double> utheta(-PI/2, PI/2);
+
+        Particle p;
+        p.position.x = ux(gen);
+        p.position.y = uy(gen);
+        p.position.z = utheta(gen);
+        p.weight = 1.0;
+
+        return p;
+    }
 };
 
-static std::random_device rd;
-static std::mt19937 gen(rd());
+
 
 
 std::vector<geometry_msgs::Point> get_laser_rays(ros::ServiceClient& laser_pol_client) {
@@ -68,6 +84,27 @@ void compute_weights(const LikelihoodField &lhf, std::array<Particle, AMOUNT_OF_
     }
 }
 
+void resample(const LikelihoodField &lhf, std::array<Particle, AMOUNT_OF_PARTICLES> &particles) {
+    constexpr double injection_probability = AMOUNT_RANDOM_INJECTIONS * PROBABILITY_RANDOM_INJECTIONS; // probably wrong
+
+    std::vector<double> masses;
+    masses.reserve(AMOUNT_OF_PARTICLES + 1);
+    for (const auto &particle : particles)
+        masses.push_back(particle.weight);
+    masses.push_back(injection_probability);
+    std::discrete_distribution<int> sampler(masses.begin(), masses.end());
+
+    std::array<Particle, AMOUNT_OF_PARTICLES> new_particles;
+    for (int i = 0; i < AMOUNT_OF_PARTICLES; i++) {
+        const int idx = sampler(gen);
+        if (idx == AMOUNT_OF_PARTICLES)
+            new_particles[i] = Particle::random(lhf);
+        else
+            new_particles[i] = particles[idx];
+    }
+    particles = new_particles;
+}
+
 int main(int argc, char **argv) {
     ros::init(argc, argv, "localize");
     ros::NodeHandle n;
@@ -85,14 +122,9 @@ int main(int argc, char **argv) {
     // init particles array
     std::array<Particle, AMOUNT_OF_PARTICLES> particles;
 
-    std::uniform_real_distribution<double> ux(0, lhf.get_col_count()*lhf.get_cell_size()/100.0);
-    std::uniform_real_distribution<double> uy(0, lhf.get_row_count()*lhf.get_cell_size()/100.0);
-    std::uniform_real_distribution<double> utheta(-PI/2, PI/2);
 
     for (int i = 0 ; i < AMOUNT_OF_PARTICLES ; i++) {
-        particles[i].position.x = ux(gen);
-        particles[i].position.y = uy(gen);
-        particles[i].position.z = utheta(gen);
+        particles[i] = Particle::random(lhf);
     }
 
     while (ros::ok()) {
@@ -100,7 +132,7 @@ int main(int argc, char **argv) {
         std::array<geometry_msgs::Point, AMOUNT_OF_PARTICLES> reference_measurements = get_laser_rays(laser_pol_client);
         compute_weights(lhf, particles, reference_measurements);
         // do sampling
-
+        resample(lhf, particles);
         // do driving
 
         // do sleep
