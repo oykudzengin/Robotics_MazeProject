f51a9e323271528733ed69ecfa5bd00c1aa1addd
maximilian <maximilian.kirst@tu-berlin.de>
2025-06-05 17:31:31 +0200

    change laser ray origin to center of robot maybe

--- src/silver_fundamentals/src/localize.cpp
+++ src/silver_fundamentals/src/localize.cpp
@@ -100,6 +100,8 @@ void compute_weights(const LikelihoodField &lhf, std::array<Particle, AMOUNT_OF_
             const double ray_weight = lhf.get_prob_field_value(global_ray_ending);
             weight *= ray_weight;
         }
+        if (weight != 1)
+            ROS_ERROR("weight set to %f for particle", weight);
         particle.weight = weight;
     }
 }
